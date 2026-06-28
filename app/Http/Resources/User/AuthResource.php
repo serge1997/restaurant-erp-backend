0e9706899ae3e2f6c80af8267b8b3814b10a4319
Lotchi Serge Gogo <serge.gogolo12@gmail.com>
2026-06-28 19:17:57 -0300

    feat: upload restaurant logo

---
+++ app/Http/Resources/User/AuthResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            ...parent::toArray($request),
+            'birth_date'    => $this->birth_date?->format("d/m/Y"),
+            'inicial'   => $this->nameInicial(),
+            'restaurant'    => [
+                'id'    => $this->restaurant_id,
+                'name'  => $this->restaurant->name,
+                'logo'  => $this->restaurant->logo,
+                'inicial'   => $this->restaurant->nameInicial()
+            ]
+        ];
+    }
+}
